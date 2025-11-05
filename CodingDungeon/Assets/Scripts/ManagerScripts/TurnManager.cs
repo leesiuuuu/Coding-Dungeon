68d783f60568e6fd4877643f9cb6c20f56b3b1b8
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-11-05 19:49:28 +0900

    턴 시스템 추가

--- CodingDungeon/Assets/Scripts/ManagerScripts/TurnManager.cs
+++ CodingDungeon/Assets/Scripts/ManagerScripts/TurnManager.cs
@@ -1,22 +1,26 @@
-using System.Runtime.InteropServices.WindowsRuntime;
-using UnityEngine;
-using UnityEngine.Events;
-
 public class TurnManager : SceneSingleMono<TurnManager>
 {
 	private int turn = 1;
 
+	// 전투 상태인지를 확인하는 변수
+	private bool isFight = false;
 
 	// 다음 턴으로 넘기는 함수
-	public void AddTurn(UnityAction onTurnAdded)
+	public void AddTurn()
 	{
 		++turn;
-		onTurnAdded?.Invoke();
 	}
 
-	// 현재 플레이어 턴인지 확인
-	public bool IsPlayerTurn()
+	// 전투 상태인지 확인할 수 있는 함수
+	public bool IsFight()
 	{
-		return (turn % 2 != 0);
+		return isFight;
+	}
+
+	// 현재 상태를 바꿔줌
+	// ex) 전투 -> 선택
+	public void ChangeAction()
+	{
+		isFight = !isFight;
 	}
 }
