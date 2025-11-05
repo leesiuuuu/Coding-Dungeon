1048c783909060707714a5cb79fcba57d12fd929
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-11-05 21:11:45 +0900

    Revert "일단 완성"
    This reverts commit f04e57f8fed0a52e724861bce0ac2c2fb882ebce.

--- CodingDungeon/Assets/Scripts/UI/CardUI.cs
+++ CodingDungeon/Assets/Scripts/UI/CardUI.cs
@@ -1,81 +1,81 @@
-//using UnityEngine;
-//using UnityEngine.UI;
-//using TMPro;
-//using CardSystem;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using CardSystem;
 
-//namespace UI
-//{
-//    public class CardUI : MonoBehaviour
-//    {
-//        [Header("UI References")]
-//        [SerializeField] private Image cardImage;
-//        [SerializeField] private TextMeshProUGUI nameText;
-//        [SerializeField] private TextMeshProUGUI descriptionText;
-//        [SerializeField] private Button cardButton;
+namespace UI
+{
+    public class CardUI : MonoBehaviour
+    {
+        [Header("UI References")]
+        [SerializeField] private Image cardImage;
+        [SerializeField] private TextMeshProUGUI nameText;
+        [SerializeField] private TextMeshProUGUI descriptionText;
+        [SerializeField] private Button cardButton;
 
-//        private AbstractCardSo<object> cardData;
-//        private System.Action<CardUI> onCardClicked;
+        private AbstractCardSo<object> cardData;
+        private System.Action<CardUI> onCardClicked;
 
-//        public AbstractCardSo<object> CardData => cardData;
+        public AbstractCardSo<object> CardData => cardData;
 
-//        private void Awake()
-//        {
-//            if (cardButton != null)
-//            {
-//                cardButton.onClick.AddListener(OnCardClicked);
-//            }
-//        }
+        private void Awake()
+        {
+            if (cardButton != null)
+            {
+                cardButton.onClick.AddListener(OnCardClicked);
+            }
+        }
 
-//        public void SetCardData(AbstractCardSo<object> card)
-//        {
-//            cardData = card;
-//            UpdateUI();
-//        }
+        public void SetCardData(AbstractCardSo<object> card)
+        {
+            cardData = card;
+            UpdateUI();
+        }
 
-//        private void UpdateUI()
-//        {
-//            if (cardData == null)
-//            {
-//                gameObject.SetActive(false);
-//                return;
-//            }
+        private void UpdateUI()
+        {
+            if (cardData == null)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
 
-//            gameObject.SetActive(true);
+            gameObject.SetActive(true);
 
-//            if (nameText != null)
-//            {
-//                nameText.text = cardData.Name;
-//            }
+            if (nameText != null)
+            {
+                nameText.text = cardData.Name;
+            }
 
-//            if (descriptionText != null)
-//            {
-//                descriptionText.text = cardData.Description;
-//            }
+            if (descriptionText != null)
+            {
+                descriptionText.text = cardData.Description;
+            }
 
-//            // 카드 이미지는 카드 데이터에 따라 설정
-//            // if (cardImage != null && cardData.Icon != null)
-//            // {
-//            //     cardImage.sprite = cardData.Icon;
-//            // }
-//        }
+            // 카드 이미지는 카드 데이터에 따라 설정
+            // if (cardImage != null && cardData.Icon != null)
+            // {
+            //     cardImage.sprite = cardData.Icon;
+            // }
+        }
 
-//        public void SetOnCardClicked(System.Action<CardUI> callback)
-//        {
-//            onCardClicked = callback;
-//        }
+        public void SetOnCardClicked(System.Action<CardUI> callback)
+        {
+            onCardClicked = callback;
+        }
 
-//        private void OnCardClicked()
-//        {
-//            onCardClicked?.Invoke(this);
-//        }
+        private void OnCardClicked()
+        {
+            onCardClicked?.Invoke(this);
+        }
 
-//        public void SetInteractable(bool interactable)
-//        {
-//            if (cardButton != null)
-//            {
-//                cardButton.interactable = interactable;
-//            }
-//        }
-//    }
-//}
+        public void SetInteractable(bool interactable)
+        {
+            if (cardButton != null)
+            {
+                cardButton.interactable = interactable;
+            }
+        }
+    }
+}
 
