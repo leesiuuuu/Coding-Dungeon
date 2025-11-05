d44cf46a511394c99fea4071dfd3d818d567f1ac
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-11-05 21:24:19 +0900

    isSelected 삭제

--- CodingDungeon/Assets/Scripts/GridMove/GridMovement.cs
+++ CodingDungeon/Assets/Scripts/GridMove/GridMovement.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class GridMovement : MonoBehaviour
 {
-	[SerializeField] private bool isSelected = false;
+	//[SerializeField] private bool isSelected = false;
 	
 	// 오브젝트 이동 함수
 	public void Move(Vector3 dir)
 	{
-		if (!isSelected) return;
+		//if (!isSelected) return;
 
 		Vector3 dest = transform.position + dir;
 		transform.DOJump(dest, 0.5f, 1, 0.2f).SetEase(Ease.OutQuad);
 	}
-	public void Select()
-	{
-		isSelected = true;
-	}
-	public void Deselect()
-	{
-		isSelected = false;
-	}
+	//public void Select()
+	//{
+	//	isSelected = true;
+	//}
+	//public void Deselect()
+	//{
+	//	isSelected = false;
+	//}
 }
