f04e57f8fed0a52e724861bce0ac2c2fb882ebce
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-11-05 21:10:20 +0900

    일단 완성

--- CodingDungeon/Assets/Scripts/GridMove/GridMovement.cs
+++ CodingDungeon/Assets/Scripts/GridMove/GridMovement.cs
@@ -5,48 +5,21 @@ using UnityEngine;
 public class GridMovement : MonoBehaviour
 {
 	[SerializeField] private bool isSelected = false;
-	[SerializeField] private GameObject selectTile;
-
-	private void Update()
-	{
-		if (isSelected)
-			showRange();
-	}
-
-
-	// 범위 보여주기 함수
-	private void showRange()
-	{
-		Vector3 point = Input.mousePosition;
-		point.z = Mathf.Abs(Camera.main.transform.position.z);
-		point = Camera.main.ScreenToWorldPoint(point);
-
-		Vector3 dir = (point - transform.position).normalized;
-
-		int x = Mathf.RoundToInt(dir.x);
-		int y = Mathf.RoundToInt(dir.y);
-
-		Vector3Int dir1 = new Vector3Int(x, y, 0);
-
-		selectTile.transform.position = transform.position + dir1;
-
-		if (Input.GetMouseButtonDown(0))
-		{
-			Move(dir1);
-		}
-	}
 	
 	// 오브젝트 이동 함수
 	public void Move(Vector3 dir)
 	{
-		selectTile.SetActive(false);
-		Vector3 dest = transform.position + dir;
+		if (!isSelected) return;
 
-		// 추후 개발 시 OnComplete 제외하기
-		// isSelected로 selectTile 관리
-		transform.DOJump(dest, 0.5f, 1, 0.2f).SetEase(Ease.OutQuad).OnComplete(() =>
-		{
-			selectTile.SetActive(true);
-		});
+		Vector3 dest = transform.position + dir;
+		transform.DOJump(dest, 0.5f, 1, 0.2f).SetEase(Ease.OutQuad);
+	}
+	public void Select()
+	{
+		isSelected = true;
+	}
+	public void Deselect()
+	{
+		isSelected = false;
 	}
 }
--- CodingDungeon/Assets/Scripts/UI/UIManager.cs
+++ CodingDungeon/Assets/Scripts/UI/UIManager.cs
@@ -1,116 +1,116 @@
-using System.Collections.Generic;
-using UnityEngine;
-using CardSystem;
-using UI;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using CardSystem;
+//using UI;
 
-public class UIManager : MonoBehaviour
-{
-    [Header("References")]
-    [SerializeField] private IngameCardModel cardModel;
-    [SerializeField] private Transform cardContainer;
-    [SerializeField] private CardUI cardPrefab;
+//public class UIManager : MonoBehaviour
+//{
+//    [Header("References")]
+//    [SerializeField] private IngameCardModel cardModel;
+//    [SerializeField] private Transform cardContainer;
+//    [SerializeField] private CardUI cardPrefab;
 
-    [Header("Profile UI")]
-    [SerializeField] private UnityEngine.UI.Image profileImage;
-    [SerializeField] private UnityEngine.UI.Image hpFillImage;
-    [SerializeField] private Character character;
+//    [Header("Profile UI")]
+//    [SerializeField] private UnityEngine.UI.Image profileImage;
+//    [SerializeField] private UnityEngine.UI.Image hpFillImage;
+//    [SerializeField] private Character character;
 
-    private List<CardUI> cardUIList = new List<CardUI>();
+//    private List<CardUI> cardUIList = new List<CardUI>();
 
-    private void Start()
-    {
-        if (cardModel != null)
-        {
-            UpdateHandUI();
-        }
+//    private void Start()
+//    {
+//        if (cardModel != null)
+//        {
+//            UpdateHandUI();
+//        }
 
-        if (character != null)
-        {
-            UpdateProfileUI();
-        }
-    }
+//        if (character != null)
+//        {
+//            UpdateProfileUI();
+//        }
+//    }
 
-    private void OnEnable()
-    {
-        // 카드 모델 변경 시 UI 업데이트를 위한 이벤트 구독
-        // 필요시 구현
-    }
+//    private void OnEnable()
+//    {
+//        // 카드 모델 변경 시 UI 업데이트를 위한 이벤트 구독
+//        // 필요시 구현
+//    }
 
-    private void OnDisable()
-    {
-        // 이벤트 구독 해제
-    }
+//    private void OnDisable()
+//    {
+//        // 이벤트 구독 해제
+//    }
 
-    public void UpdateHandUI()
-    {
-        if (cardModel == null || cardContainer == null)
-        {
-            return;
-        }
+//    public void UpdateHandUI()
+//    {
+//        if (cardModel == null || cardContainer == null)
+//        {
+//            return;
+//        }
 
-        // 기존 카드 UI 제거
-        foreach (var cardUI in cardUIList)
-        {
-            if (cardUI != null)
-            {
-                Destroy(cardUI.gameObject);
-            }
-        }
-        cardUIList.Clear();
+//        // 기존 카드 UI 제거
+//        foreach (var cardUI in cardUIList)
+//        {
+//            if (cardUI != null)
+//            {
+//                Destroy(cardUI.gameObject);
+//            }
+//        }
+//        cardUIList.Clear();
 
-        // 새로운 카드 UI 생성
-        foreach (var card in cardModel.Hand)
-        {
-            if (cardPrefab != null && card != null)
-            {
-                CardUI cardUI = Instantiate(cardPrefab, cardContainer);
-                cardUI.SetCardData(card);
-                cardUI.SetOnCardClicked(OnCardClicked);
-                cardUIList.Add(cardUI);
-            }
-        }
-    }
+//        // 새로운 카드 UI 생성
+//        foreach (var card in cardModel.Hand)
+//        {
+//            if (cardPrefab != null && card != null)
+//            {
+//                CardUI cardUI = Instantiate(cardPrefab, cardContainer);
+//                cardUI.SetCardData(card);
+//                cardUI.SetOnCardClicked(OnCardClicked);
+//                cardUIList.Add(cardUI);
+//            }
+//        }
+//    }
 
-    private void OnCardClicked(CardUI cardUI)
-    {
-        if (cardUI?.CardData != null && cardModel != null)
-        {
-            cardModel.UseCard(cardUI.CardData);
-            UpdateHandUI();
-        }
-    }
+//    private void OnCardClicked(CardUI cardUI)
+//    {
+//        if (cardUI?.CardData != null && cardModel != null)
+//        {
+//            cardModel.UseCard(cardUI.CardData);
+//            UpdateHandUI();
+//        }
+//    }
 
-    public void UpdateProfileUI()
-    {
-        if (character == null)
-        {
-            return;
-        }
+//    public void UpdateProfileUI()
+//    {
+//        if (character == null)
+//        {
+//            return;
+//        }
 
-        // HP 바 업데이트
-        if (hpFillImage != null)
-        {
-            float hpPercentage = (float)character.CurrentHP / character.MaxHP;
-            hpFillImage.fillAmount = hpPercentage;
-        }
+//        // HP 바 업데이트
+//        if (hpFillImage != null)
+//        {
+//            float hpPercentage = (float)character.CurrentHP / character.MaxHP;
+//            hpFillImage.fillAmount = hpPercentage;
+//        }
 
-        // 프로필 이미지는 필요시 구현
-        // if (profileImage != null && character.ProfileSprite != null)
-        // {
-        //     profileImage.sprite = character.ProfileSprite;
-        // }
-    }
+//        // 프로필 이미지는 필요시 구현
+//        // if (profileImage != null && character.ProfileSprite != null)
+//        // {
+//        //     profileImage.sprite = character.ProfileSprite;
+//        // }
+//    }
 
-    public void SetCardModel(IngameCardModel model)
-    {
-        cardModel = model;
-        UpdateHandUI();
-    }
+//    public void SetCardModel(IngameCardModel model)
+//    {
+//        cardModel = model;
+//        UpdateHandUI();
+//    }
 
-    public void SetCharacter(Character targetCharacter)
-    {
-        character = targetCharacter;
-        UpdateProfileUI();
-    }
-}
+//    public void SetCharacter(Character targetCharacter)
+//    {
+//        character = targetCharacter;
+//        UpdateProfileUI();
+//    }
+//}
 
