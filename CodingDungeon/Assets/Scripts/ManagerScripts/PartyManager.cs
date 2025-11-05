db5175781f1951b4c7c11cd39cee54bef57acb43
ChanYoungOh <163986958+oty0365@users.noreply.github.com>
2025-11-06 03:21:48 +0900

    wip

--- CodingDungeon/Assets/Scripts/ManagerScripts/PartyManager.cs
+++ CodingDungeon/Assets/Scripts/ManagerScripts/PartyManager.cs
@@ -7,7 +7,6 @@ public class PartyManager : SceneSingleMono<PartyManager>
     public List<Character> Characters;
     [SerializeField] private Character _selectedCharacter;
     public Character SelectedCharacter => _selectedCharacter;
-    private int _characterLimitIndex = 0;
     public void OnSelectCharacter(Character character)
     {
         _selectedCharacter = character;
