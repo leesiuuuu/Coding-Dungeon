bde6b40b270b77d191f41076d41bb8ab4487ae8f
ChanYoungOh <163986958+oty0365@users.noreply.github.com>
2025-11-06 08:41:58 +0900

    왼쪽정렬

--- CodingDungeon/Assets/Scripts/ManagerScripts/ActiveQueueUISystem.cs
+++ CodingDungeon/Assets/Scripts/ManagerScripts/ActiveQueueUISystem.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -31,16 +32,106 @@ public class ActiveQueueUISystem : CardHandSystem
 
 	protected new void RecalculatePositions()
 	{
-		base.RecalculatePositions();
+		targetPositions.Clear();
+    
+		int cardCount = cards.Count;
+    
+		for (int i = 0; i < cardCount; i++)
+		{
+			Vector3 pos = handCenter.position + new Vector3(i * pointDistance, 0, 0);
+			targetPositions.Add(pos);
+		}
 	}
 	
 	protected new IEnumerator SlideInCard(Transform card, int index)
 	{
-		return base.SlideInCard(card, index);
+		if (index >= targetPositions.Count) yield break;
+    
+		Vector3 targetPos = targetPositions[index];
+		Vector3 startPos = targetPos + new Vector3(slideInDistance, 0, 0);
+    
+		card.position = startPos;
+		card.gameObject.SetActive(true);
+    
+		float elapsed = 0f;
+    
+		List<Vector3> oldPositions = new List<Vector3>();
+		for (int i = 0; i < cards.Count - 1; i++)
+		{
+			if (cards[i] != null)
+				oldPositions.Add(cards[i].transform.position);
+		}
+    
+		while (elapsed < slideInDuration)
+		{
+			elapsed += Time.deltaTime;
+			float t = elapsed / slideInDuration;
+			float curveValue = slideInCurve.Evaluate(t);
+        
+			if (index < targetPositions.Count)
+			{
+				targetPos = targetPositions[index];
+				card.position = Vector3.Lerp(startPos, targetPos, curveValue);
+			}
+			
+			for (int i = 0; i < cards.Count - 1; i++)
+			{
+				if (cards[i] != null && i < targetPositions.Count && i < oldPositions.Count)
+				{
+					cards[i].transform.position = Vector3.Lerp(oldPositions[i], targetPositions[i], curveValue);
+				}
+			}
+        
+			yield return null;
+		}
+    
+		for (int i = 0; i < cards.Count; i++)
+		{
+			if (cards[i] != null && i < targetPositions.Count)
+			{
+				cards[i].transform.position = targetPositions[i];
+			}
+		}
+    
+		isAddingCard = false;
 	}
 
 	protected new IEnumerator RepositionExistingCards()
 	{
-		return base.RepositionExistingCards();
+		float duration = 0.3f;
+		float elapsed = 0f;
+    
+		List<Vector3> startPositions = new List<Vector3>();
+		foreach (var card in cards)
+		{
+			if (card != null)
+				startPositions.Add(card.transform.position);
+		}
+    
+		while (elapsed < duration)
+		{
+			elapsed += Time.deltaTime;
+			float t = Mathf.Clamp01(elapsed / duration);
+			float curveValue = slideInCurve.Evaluate(t);
+        
+			for (int i = 0; i < cards.Count; i++)
+			{
+				if (cards[i] != null && i < targetPositions.Count && i < startPositions.Count)
+				{
+
+					cards[i].transform.position = Vector3.Lerp(startPositions[i], targetPositions[i], curveValue);
+				}
+			}
+        
+			yield return null;
+		}
+
+		for (int i = 0; i < cards.Count; i++)
+		{
+			if (cards[i] != null && i < targetPositions.Count)
+			{
+				cards[i].transform.position = targetPositions[i];
+			}
+		}
 	}
 }
