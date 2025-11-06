08beb57c7add63d061ad8aefbaaff99d3891385a
jyjun_080808 <jinyj0217@gmail.com>
2025-11-07 03:30:13 +0900

    EntityStatusEffectListUI

---
+++ CodingDungeon/Assets/Scripts/UIScripts/EntityStatusEffectListUI.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class EntityStatusEffectListUI : MonoBehaviour
+{
+	public TextMeshProUGUI Text;
+
+	[SerializeField]
+	private GameObject _entity;
+
+	public IEntity Entity => _entity.GetComponent<IEntity>();
+
+	public void Update()
+	{
+		string list = "";
+		foreach (var i in Entity.Status.CurrentEffects.ToList())
+		{
+			string viewString = i.GetViewString();
+			if (!String.IsNullOrEmpty(viewString))
+			{
+				list += "\n" + viewString;
+			}
+		}
+		Text.text = list;
+	} 
+}
