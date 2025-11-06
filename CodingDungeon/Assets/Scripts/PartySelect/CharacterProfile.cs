766b897f84006cefa442a042f8d881a03111c34e
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-11-06 10:57:16 +0900

    캐릭터 파티 선택 UI 추가

--- CodingDungeon/Assets/Scripts/PartySelect/CharacterProfile.cs
+++ CodingDungeon/Assets/Scripts/PartySelect/CharacterProfile.cs
@@ -1,52 +1,55 @@
-using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CharacterProfile : MonoBehaviour
 {
-    [Header("Profile UI")]
-    [SerializeField] private CharacterSo character;
-    [SerializeField] private TMP_Text title;
-    [SerializeField] private Image profile;
+	[Header("Profile UI")]
+	[SerializeField] private PartySO partySO;
+	[SerializeField] private TMP_Text title;
+	[SerializeField] private Image profile;
 
-    [Header("Stat UI")]
+	[Header("Stat UI")]
 	[SerializeField] private TMP_Text Attack;
 	[SerializeField] private TMP_Text Defence;
 	[SerializeField] private TMP_Text HP;
-
 	[SerializeField] private GameObject selectedObj;
 
-    private CharacterAttributes characterAttributes;
-    private bool isSelected = false;
-    void Start()
-    {
-        characterAttributes = character.Attributes;
+	private CharacterSo character;
+	private CharacterAttributes characterAttributes;
+	private GameObject prefab;
+	private bool isSelected = false;
 
-        title.text = character.Name;
-        profile.sprite = character.Image;
-
+	void Start()
+	{
+		character = partySO.character;
+		prefab = partySO.Prefab;
+		characterAttributes = character.Attributes;
+		title.text = character.Name;
+		profile.sprite = character.Image;
 		Attack.text = characterAttributes.DamageModifier + "x";
 		Defence.text = characterAttributes.DefenseModifier + "x";
 		HP.text = characterAttributes.MaxHp.ToString();
 	}
 
-    public void OnClicked()
-    {
-        if (PartySelector.Instance.IsInclude(this))
-        {
-            PartySelector.Instance.Deselect(this);
-            return;
-        }
+	public void OnClicked()
+	{
+		if (PartySelector.Instance.IsInclude(this))
+		{
+			PartySelector.Instance.Deselect(this);
+			return;
+		}
 
-        if (PartySelector.Instance.IsFull())
-        {
-            PartySelector.Instance.Dequeue();
-        }
-        PartySelector.Instance.Enqueue(this);
-    }
-    public void Select()
-    {
+		if (PartySelector.Instance.IsFull())
+		{
+			PartySelector.Instance.Dequeue();
+		}
+
+		PartySelector.Instance.Enqueue(this);
+	}
+
+	public void Select()
+	{
 		isSelected = true;
 		selectedObj.SetActive(isSelected);
 	}
@@ -57,4 +60,5 @@ public class CharacterProfile : MonoBehaviour
 		selectedObj.SetActive(isSelected);
 	}
 
+	public GameObject GetPrefab() => prefab;
 }
