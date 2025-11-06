60d1bfc817b305def1eb683a37119e57ccfc0a72
ChanYoungOh <163986958+oty0365@users.noreply.github.com>
2025-11-06 15:43:18 +0900

    wip

--- CodingDungeon/Assets/Scripts/GridMove/GridMovement.cs
+++ CodingDungeon/Assets/Scripts/GridMove/GridMovement.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class GridMovement : MonoBehaviour
 {
+	[SerializeField] AudioClip _audioClip;
 	public void Move(Vector3 dir)
 	{
 		Vector3 dest = transform.position + dir;
+		SoundManager.Instance.SFXPlay("SFX", _audioClip);
 		transform.DOJump(dest, 0.5f, 1, 0.2f).SetEase(Ease.OutQuad);
 	}
 }
