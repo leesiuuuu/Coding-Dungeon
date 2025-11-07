856c5bb88419485be629480a62ad1060d9ab8108
jyjun_080808 <jinyj0217@gmail.com>
2025-11-07 09:09:05 +0900

    FreeOperationCard

---
+++ CodingDungeon/Assets/Scripts/Card/Impl/Operation/SimpleOperationCard.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Coding Dungeon/Operation Card/Simple")]
+public class SimpleOperationCard : OperationCardSo
+{
+	public override void StartAction(CardActionContext ctx)
+	{
+	}
+}
